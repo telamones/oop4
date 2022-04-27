4595522659819c36164addf3b025372914de2637
telamones <vvtatatavv@gmail.com>
2022-04-27 10:21:32 +0300

    chang

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     float a,b;
     float n,m;
     bool err=0;
+
     cout << "enter a:" << endl << "enter b:" << endl;
     cin >> a >> b;
 
@@ -21,6 +22,11 @@ int main() {
     } else {
         n = pow(a - b, 1.0 / 3);
     }
+
+    if (pow(sin(a), 2) - cos(n) < 0) {
+        err=1;
+    }
+
 if (err==0) {
     if (n < b) {
         m = (n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(n));
@@ -29,10 +35,10 @@ if (err==0) {
     } else {
         m = pow(n, 3.0) + n * a * a;
     }
-
     cout << "a= " << a << endl << "b= " << b << endl << "n= " << n << endl << "m= " << m << endl;
 } else {
     cout << "error" << endl;
 }
+
     return 0;
 }
