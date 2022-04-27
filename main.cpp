f41fbf68e3fa3fd60b26cb798bf9e78c279d7a55
telamones <vvtatatavv@gmail.com>
2022-04-27 10:10:07 +0300

    changes

--- main.cpp
+++ main.cpp
@@ -6,30 +6,33 @@ int main() {
 
     float a,b;
     float n,m;
+    bool err=0;
     cout << "enter a:" << endl << "enter b:" << endl;
     cin >> a >> b;
 
     if (a < b) {
         if (a == 0) {
-            cout << "error" << endl;
+            err=1;
         } else if (b == 0) {
-            cout << "error" << endl;
+            err=1;
         } else {
             n = a * a + (a - b) / (sin(a * b));
         }
     } else {
         n = pow(a - b, 1.0 / 3);
     }
-
-    if (n<b) {
-        m = (n+a)/(-b) + sqrt (pow(sin(a), 2) - cos(n));
-    } else if (n==b) {
-        m = b*b + tan (n*a);
+if (err==0) {
+    if (n < b) {
+        m = (n + a) / (-b) + sqrt(pow(sin(a), 2) - cos(n));
+    } else if (n == b) {
+        m = b * b + tan(n * a);
     } else {
-        m = pow (n, 3.0) + n*a*a;
+        m = pow(n, 3.0) + n * a * a;
     }
 
     cout << "a= " << a << endl << "b= " << b << endl << "n= " << n << endl << "m= " << m << endl;
-
+} else {
+    cout << "error" << endl;
+}
     return 0;
 }
